d086f34243adc0f17ab568f0a7933ebc788ec8b1
fawhn <saigonoyoake@gmail.com>
2023-06-18 23:49:46 +0300

    auth

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,6 +3,11 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
+const validateEmail = (email: string): boolean => {
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 @Schema()
 export class User {
   constructor(name: string, password: string, roles: string[]) {
@@ -11,11 +16,20 @@ export class User {
     this.roles = roles;
   }
 
-  @Prop({ required: true, unique: true })
+  @Prop({
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 16,
+  })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, trim: true })
   password: string;
+
+  @Prop({ trim: true, validate: [validateEmail] })
+  email: string;
 
   @Prop({ default: ['USER'] })
   roles: string[];
